7116161a1598ada49bf4acd79af496186aeb6ac2
Ruhama Yared <ruhamayared1@gmail.com>
2023-01-13 00:43:16 -0500

    final commit

--- src/router/actions.js
+++ src/router/actions.js
@@ -5,7 +5,6 @@ import URL from "./url"
 export async function createAction({ request }) {
   const formData = await request.formData()
 
-  //   console.log(formData.get("tasty"))
   const newCheese = {
     name: formData.get("name"),
     countryOfOrigin: formData.get("countryOfOrigin"),
